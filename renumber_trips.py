40f12d2ed6eba742d69156bbc30888ec7ed59f44
Suzanne Childress <childresssuzanne@gmail.com>
2015-02-26 14:10:50 -0800

    checking in final version of trip renumbering

--- renumber_trips.py
+++ renumber_trips.py
@@ -6,13 +6,13 @@ import process_survey as ps
 
 # We will still need to reset to trip ids on the trip file by concatenating the fields
 # new_trip_num and personID
-
+base_path = r'C:\Users\Suzanne\Documents\HouseholdSurvey\renumbertrips'
 # Load the survey data per process_survey.py
 trip = ps.load_data(config.trip_file)
 person = ps.load_data(config.person_file)
 household = ps.load_data(config.household_file)
 
-
+new_weights = pd.read_csv(base_path + '\\hh_ipfweights2.dat', delim_whitespace = True)
 
 trip.sort(['personID', 'tripID'], inplace=True)
 
@@ -26,7 +26,10 @@ trip['new_trip_num'] = persons_trips.cumcount()+1
 trip_newindex = trip.set_index('personID', drop=False)
 trip_newindex['new_total_trip'] = persons_trips.size()
 
-trip_newindex.to_csv(base_path + "\\new_trip_out.csv")
+# put the new weights on the trip file
+trip_newindex =  pd.merge(trip_newindex, new_weights, left_on = 'hhid', right_on = 'hhid', how = 'left')
+
+trip_newindex.to_csv(base_path + "\\4_PSRC2014_Trip_2014-08-07_v2-9_linked.csv", encoding = 'utf-8')
 
 
 persons_tottrips = pd.DataFrame()
@@ -38,7 +41,10 @@ persons_tottripsnewix ['numtrips']= persons_trips.size()
 # Update the persons table with the new number of trips
 person_newnumtrips = pd.merge(person, persons_tottripsnewix, left_on = 'personid', right_on = 'personID', how = 'left')
 
-person_newnumtrips.to_csv(base_path + '\\new_persons_out.csv', encoding = 'utf-8')
+# put the new weights on the trip file
+person_newnumtrips =  pd.merge(person_newnumtrips, new_weights, left_on = 'hhid', right_on = 'hhid', how = 'left')
+
+person_newnumtrips.to_csv(base_path + '\\3_PSRC2014_Person_2014-08-07_v2-3.csv', encoding = 'utf-8')
 
 #update the households table with the sum of the person trips in the
 #household
@@ -57,8 +63,8 @@ hh_newtrips.set_index('hhid', drop=False, inplace=True)
 hh_newtrips['new_hh_trip']=hh_trips_grouped.size()
 
 hh_newnumtrips = pd.merge(hh_newtrips, household, left_on = 'hhid', right_on = 'hhid', how = 'right')
-
-hh_newnumtrips.to_csv(base_path + '\\new_households_out.csv', encoding = 'utf-8')
+hh_newnumtrips = pd.merge(hh_newnumtrips, new_weights, left_on = 'hhid', right_on = 'hhid', how = 'right')
+hh_newnumtrips.to_csv(base_path + '\\1_PSRC2014_HH_2014-08-07_v2-3.csv', encoding = 'utf-8')
 
 
  
